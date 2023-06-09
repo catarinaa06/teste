83894dee2d67ed71fbdff023c3e638bf7a03f268
catarinaa06 <136069728+catarinaa06@users.noreply.github.com>
2023-06-09 12:30:11 +0100

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+	import FlourishScroller from '../components/FlourishScroller.svelte';
+	import NewsArticle from './NewsArticle.svelte';
+	let steps = [
+		'Hello! this is a step in the scroller.',
+		'And you can add as many as you want.',
+		'You only need to make sure you have enough Flourish slides to match',
+		'Pretty cool, right?',
+		'Oh, and did I mention you can also add pure HTML in the steps?',
+		'Like this <a href="https://www.google.com">link</a>.'
+	];
+</script>
+
+<NewsArticle />
+
+<FlourishScroller flourish_id="1568021" {steps} />
+
+
